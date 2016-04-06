2abaf9eb7ebdacbbda8bd69d15fe990a6b663f4d
MKrsic <krsicmatija@gmail.com>
2016-04-06 22:17:19 +0200

    Prvi (test) commit
    Commitani samo fileovi na kojima sam radio nešto

---
+++ app/src/main/java/hr/mc2/foodhub/MainActivity.java
@@ -0,0 +1,13 @@
+package hr.mc2.foodhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/hr/mc2/foodhub/SplashScreen.java
@@ -0,0 +1,34 @@
+package hr.mc2.foodhub;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Matija on 3.4.2016..
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int splashDelay = 1500;
+        setContentView(R.layout.splash);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        // Micanje action bara
+        actionBar.hide();
+        // Pokretanje MainActivity-a nakon odgode od splashDelay sekundi
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashDelay);
+    }
+
+
+}
