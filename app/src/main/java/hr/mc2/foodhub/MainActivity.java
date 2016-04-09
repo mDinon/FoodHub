a6617ad72ab5aaf00c826a5eb356a03c21d7d9ba
MKrsic <krsicmatija@gmail.com>
2016-04-09 15:59:45 +0200

    Dodana RatingStar

--- app/src/main/java/hr/mc2/foodhub/MainActivity.java
+++ app/src/main/java/hr/mc2/foodhub/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     ItemListPodaci objekt1 = new ItemListPodaci("Ime objekta 1", "Vrsta objekta 1", 100, R.drawable.logo);
     ItemListPodaci objekt2 = new ItemListPodaci("Ime objekta 2", "Vrsta objekta 2", 50, R.drawable.logo);
     ItemListPodaci objekt3 = new ItemListPodaci("Ime objekta 3", "Vrsta objekta 3", 25, R.drawable.logo);
+    ItemListPodaci objekt4 = new ItemListPodaci("Ime objekta 4", "Vrsta objekta 4", 100, R.drawable.logo);
+    ItemListPodaci objekt5 = new ItemListPodaci("Ime objekta 5", "Vrsta objekta 5", 50, R.drawable.logo);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         Restorani.add(objekt1);
         Restorani.add(objekt2);
         Restorani.add(objekt3);
+        Restorani.add(objekt4);
+        Restorani.add(objekt5);
+        Restorani.add(objekt1);
+        Restorani.add(objekt2);
+        Restorani.add(objekt3);
+        Restorani.add(objekt4);
+        Restorani.add(objekt5);
 
         // Kreiranje array liste custom klase i popunjavanje iste
         ArrayList<ItemListPodaci> objekti = new ArrayList<>();
