9e25ab5a8c167e69a0f3e621383a937a7966a7b4
mlastovcic <matijalas@gmail.com>
2016-04-11 12:10:56 +0200

    Menu Acitivity datoteke
    Ubacio sam klase i layout datoteke.
    Nisam još implementiro tabove i swipe pa su trenurno gumbi. Prvo ću
    složiti 3 fragmenta
    Prvi zasad pokazuje logo i ime resorana il čega već
    a drugi zasad niš, sadržava prazni list view, koji će pokazivati cjenik

---
+++ app/src/main/java/hr/mc2/foodhub/MenuActivity.java
@@ -0,0 +1,31 @@
+package hr.mc2.foodhub;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Fragment foodFragment = new FoodFragment();
+        Fragment aboutFragment = new AboutFragment();
+
+        Bundle data = new Bundle();
+        data.putString("name", "Ime_lokala_restorana_cega vec");
+        data.putInt("logo", R.drawable.test_logo);
+        aboutFragment.setArguments(data);
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, aboutFragment);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+
+}
