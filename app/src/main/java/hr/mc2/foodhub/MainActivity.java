bdba5bf7dbcafe4d4b3b207adf9f7fbebd66a352
MKrsic <krsicmatija@gmail.com>
2016-04-14 11:55:00 +0200

    Moram ovo commitat da mogu syncat

--- app/src/main/java/hr/mc2/foodhub/MainActivity.java
+++ app/src/main/java/hr/mc2/foodhub/MainActivity.java
@@ -24,29 +24,41 @@ public class MainActivity extends AppCompatActivity {
 
     public ArrayList<ItemListPodaci> Restorani = new ArrayList<>();
 
-    ItemListPodaci objekt1 = new ItemListPodaci("A_Ime objekta 1", "Vrsta objekta 1", 100, R.drawable.logo);
-    ItemListPodaci objekt2 = new ItemListPodaci("B_Ime objekta 2", "Vrsta objekta 2", 50, R.drawable.logo);
-    ItemListPodaci objekt3 = new ItemListPodaci("C_Ime objekta 3", "Vrsta objekta 3", 25, R.drawable.logo);
-    ItemListPodaci objekt4 = new ItemListPodaci("AAIme objekta 4", "Vrsta objekta 4", 100, R.drawable.logo);
-    ItemListPodaci objekt5 = new ItemListPodaci("BBIme objekta 5", "Vrsta objekta 5", 50, R.drawable.logo);
+    ItemListPodaci objekt1 = new ItemListPodaci("Mlinar", "Pekara", 50, R.drawable.logo);
+    ItemListPodaci objekt2 = new ItemListPodaci("Samoborska pekara", "Pekara", 40, R.drawable.logo);
+    ItemListPodaci objekt3 = new ItemListPodaci("Slastičarnica Nebi", "Slastičarnica", 30, R.drawable.logo);
+    ItemListPodaci objekt4 = new ItemListPodaci("Rubelj", "Pečenjara", 20, R.drawable.logo);
+    ItemListPodaci objekt5 = new ItemListPodaci("Šabarić", "Pečenjara", 10, R.drawable.logo);
+    ItemListPodaci objekt6 = new ItemListPodaci("Napoli", "Pizzeria", 100, R.drawable.logo);
+    ItemListPodaci objekt7 = new ItemListPodaci("Pizzeria Buldog", "Pizzeria", 90 , R.drawable.logo);
+    ItemListPodaci objekt8 = new ItemListPodaci("Irish pub Scout", "Pub", 80, R.drawable.logo);
+    ItemListPodaci objekt9 = new ItemListPodaci("Havanna", "Caffe bar", 70, R.drawable.logo);
+    ItemListPodaci objekt10 = new ItemListPodaci("Ara", "Caffe bar", 60, R.drawable.logo);
+
+    ItemListPodaci objekt11 = new ItemListPodaci("Aaa", "Caffe bar", 60, R.drawable.logo);
+    ItemListPodaci objekt12 = new ItemListPodaci("Abb", "Caffe bar", 60, R.drawable.logo);
+    ItemListPodaci objekt13 = new ItemListPodaci("Aba", "Caffe bar", 60, R.drawable.logo);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         Restorani.add(objekt1);
         Restorani.add(objekt2);
         Restorani.add(objekt3);
         Restorani.add(objekt4);
         Restorani.add(objekt5);
-        Restorani.add(objekt1);
-        Restorani.add(objekt2);
-        Restorani.add(objekt3);
-        Restorani.add(objekt4);
-        Restorani.add(objekt5);
-
-        //objekt1.bubble_srt_rating(Restorani);
+        Restorani.add(objekt6);
+        Restorani.add(objekt7);
+        Restorani.add(objekt8);
+        Restorani.add(objekt9);
+        Restorani.add(objekt10);
+        Restorani.add(objekt11);
+        Restorani.add(objekt12);
+        Restorani.add(objekt13);
 
         // Kreiranje array liste custom klase i popunjavanje iste
         ArrayList<ItemListPodaci> objekti = new ArrayList<>();
@@ -68,14 +80,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-/*        bubbleSrtRatingDesc(Restorani);
-        // Kreiranje adaptera koji konvertira polje u view
-        ItemListAdapter adapter = new ItemListAdapter(this,Restorani);
-        // Postavljanje adaptera u listview
-        ListView listView = (ListView) findViewById(R.id.listView2);
-        listView.setAdapter(adapter);
-        return true;*/
-
         //handle presses on the action bar items
         // Kreiranje adaptera koji konvertira polje u view
         ItemListAdapter adapter = new ItemListAdapter(this,Restorani);
@@ -125,7 +129,8 @@ public class MainActivity extends AppCompatActivity {
         for (int m = n; m >= 0; m--) {
             for (int i = 0; i < n - 1; i++) {
                 k = i + 1;
-                if (podaci.get(i).getImeObjekta().compareTo(podaci.get(k).getImeObjekta()) > 0) {
+                if (podaci.get(i).getImeObjekta().valueOf(podaci.get(i).getImeObjekta().charAt(0)).toLowerCase().
+                        compareTo(podaci.get(k).getImeObjekta().valueOf(podaci.get(k).getImeObjekta().charAt(0)).toLowerCase()) > 0) {
                     swapNumbers(i, k, podaci);
                 }
             }
